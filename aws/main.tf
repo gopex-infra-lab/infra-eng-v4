7ee1a9c3b9f7fbe3b776b40a95c9f60cffc5dc69
Miguel Gonzalez <gomiguel82@gmail.com>
2026-04-22 09:10:54 -0400

    adjust aws config

--- aws/main.tf
+++ aws/main.tf
@@ -50,6 +50,14 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["173.179.47.202/32"]
   }
 
+  ingress {
+    description = "FastAPI"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
